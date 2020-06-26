cf8ea18848cdb37916d25b86cfa9fde3a2381bea
aydanhvd <aydanhaqverdili@outlook.com>
2020-06-26 16:06:57 +0400

    //Home screen created shaering post and navigating sceens done

--- student-information-system/components/index.js
+++ student-information-system/components/index.js
@@ -1,12 +1,17 @@
 export { CustomText } from './CustomText';
 export { Header } from './Header';
 export { Footer } from './Footer';
+
 export { HomeScreenField } from './HomeScreenField';
-export { HomeScrenPost } from './HomeScreenPost';
-export {HomeScreenHeader} from './HomeScreenHeader'
+export { HomeScreenPosts } from './HomeScreenPosts';
+export { HomeScreenHeader } from './HomeScreenHeader';
+export { HomeScreenPostBubble } from './HomeScreenPostBubble';
+
 export { ClassField } from './ClassField';
 export { GradeShower } from './GradeShower';
 export { Seperator } from './Seperator';
+export {MaterialsScreenField} from './MaterialsScreenField';
+
 export { Information } from './Information';
 export { ProgressBar } from './ProgressBar';
 export { ScheduleField } from './ScheduleField';
