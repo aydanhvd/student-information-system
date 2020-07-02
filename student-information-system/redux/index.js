180fac2bc2e4e28dcb6198c4b77fc2b31bf6c8f7
rashad2001 <azimovrasad@gmail.com>
2020-07-03 00:18:34 +0400

    persistor

--- student-information-system/redux/index.js
+++ student-information-system/redux/index.js
@@ -1,13 +1,43 @@
+
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import { persistStore, persistReducer } from 'redux-persist'
+
+import { AsyncStorage } from "react-native";
+
 
 import { MODULE_NAME as postsModuleName, reducer as postsReducer } from './posts';
 import { MODULE_NAME as authModuleName, reducer as authReducer } from './auth';
-const reducer = combineReducers({
+
+
+const config = {
+	key: "root",
+	storage: AsyncStorage,
+  };
+  const reducer = combineReducers({
 	[postsModuleName]: postsReducer,
 	[authModuleName]: authReducer
 });
-const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)));
+  const rootPersistReducer = persistReducer(config,reducer);
 
+  const store = createStore(
+	rootPersistReducer,
+	composeWithDevTools(applyMiddleware(thunk))
+  );
+export const persistor = persistStore(store);
 export default store;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
