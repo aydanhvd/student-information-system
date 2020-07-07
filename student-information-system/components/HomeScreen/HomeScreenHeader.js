fa03ce7380226b6c2bbdb816c3d43831d6d85602
aydanhvd <aydanhaqverdili@outlook.com>
2020-07-07 15:17:46 +0400

    you can now only post on news or your own class

--- student-information-system/components/HomeScreen/HomeScreenHeader.js
+++ student-information-system/components/HomeScreen/HomeScreenHeader.js
@@ -4,43 +4,49 @@ import { connect } from 'react-redux';
 
 import { COLORS } from '../../styles/colors';
 import { CustomText } from '../Customs/CustomText';
-import { setActivePosts, selectActivePosts, selectPosts } from '../../redux/posts';
-import {GLOBAL_STYLES} from "../../styles";
+import { setActivePosts, selectActivePosts} from '../../redux/posts';
+import { GLOBAL_STYLES } from '../../styles';
+import { selectAuthGroup } from '../../redux/auth';
 
 const mapStateToProps = (state) => ({
 	activePostID: selectActivePosts(state),
+	group: selectAuthGroup(state) //ID of the groups user
 });
 
 export const HomeScreenHeader = connect(mapStateToProps, {
 	setActivePosts
-})(({ feeds, setActivePosts, activePostID }) => {
+})(({ feeds, setActivePosts, activePostID, group }) => {
 	return (
-		<View style={styles.container}>
-			<FlatList
-				contentContainerStyle={styles.container}
-				data={feeds} //BTNS SHOULD BE FILTERED ACCORDING TO USER ACSESS
-				renderItem={({ item }) => {
-					return (
-						<TouchableOpacity
-							style={styles.btn}
-							onPress={() => {
-								setActivePosts(item.ID);
-							}}
-						>
-							<CustomText
-								style={{
-									...styles.btnText,
-									color:(activePostID === item.ID ) ? COLORS.acsentColor : COLORS.acsentLight
+		<FlatList
+			contentContainerStyle={styles.container}
+			data={feeds} //BTNS SHOULD BE FILTERED ACCORDING TO USER ACSESS
+			renderItem={({ item }) => {
+				return (
+					<>
+						{(item.ID === group || item.feed === 'News') && (
+							<TouchableOpacity
+								style={styles.btn}
+								onPress={() => {
+									setActivePosts(item.ID);
 								}}
 							>
-								{item.feed}
-							</CustomText>
-							{(activePostID === item.ID ) && <View style={styles.indicator} />}
-						</TouchableOpacity>
-					);
-				}}
-			/>
-		</View>
+								<CustomText
+									style={{
+										...styles.btnText,
+										color: activePostID === item.ID ? COLORS.acsentColor : COLORS.acsentLight
+									}}
+								>
+									{item.feed}
+								</CustomText>
+								{/* {(activePostID === item.ID) ? <View style={styles.indicator} /> : <View/>} */}
+								{/* i dont know why but indicator doesnt work */}
+							</TouchableOpacity>						
+						)}
+						
+					</>
+				);
+			}}
+		/>
 	);
 });
 
@@ -48,9 +54,10 @@ const styles = StyleSheet.create({
 	container: {
 		backgroundColor: COLORS.backgroundLight,
 		height: 60,
+		width: '100%',
 		flexDirection: 'row',
 		alignItems: 'center',
-		justifyContent: 'space-between',
+		justifyContent: 'flex-end'
 	},
 	btnText: {
 		fontSize: 18,
