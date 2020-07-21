0c18a41ada662d21c811242d8f9a2bd9f8e3bd15
rashad2001 <azimovrasad@gmail.com>
2020-07-21 14:30:23 +0400

    adding spinner to MessageScreen

--- student-information-system/components/MessageScreen/UserCard.js
+++ student-information-system/components/MessageScreen/UserCard.js
@@ -1,61 +1,100 @@
-import React from 'react';
-import { StyleSheet, Image, TouchableOpacity } from 'react-native';
-import { COLORS, ICONS_LIGHT } from '../../styles';
-import { CustomText } from '../../commons/CustomText';
-import {selectTheme} from "../../redux/theme";
-import {connect} from "react-redux";
+import React from "react";
+import { StyleSheet, Image, TouchableOpacity,View } from "react-native";
+import { COLORS, ICONS_LIGHT } from "../../styles";
+import { CustomText } from "../../commons/CustomText";
+import { selectTheme } from "../../redux/theme";
+import { connect } from "react-redux";
+import { Spinner } from "@ui-kitten/components";
 
 const mapStateToProps = (state) => ({
-	darkMode: selectTheme(state)
+  darkMode: selectTheme(state),
 });
+const DATA = new Array(8).fill({});
 
-export const UserCard = connect(mapStateToProps, {})(({ user, onPress, darkMode }) => {
+export const UserCard = connect(
+  mapStateToProps,
+  {}
+)(({ user, onPress, darkMode }) => {
+  const colorTheme = darkMode
+    ? {
+        backgroundColor: COLORS.backgroundDark,
+        userColor: COLORS.sendDark,
+        nameColor: COLORS.backgroundLight,
+      }
+    : {
+        backgroundColor: COLORS.backgroundLight,
+        userColor: COLORS.drawerLight,
+        nameColor: COLORS.acsentLight,
+      };
+  const [data, setData] = React.useState([]);
 
-	const colorTheme = darkMode
-		? {
-			backgroundColor: COLORS.backgroundDark,
-			userColor: COLORS.sendDark,
-			nameColor: COLORS.backgroundLight
-		} : {
-			backgroundColor: COLORS.backgroundLight,
-			userColor: COLORS.drawerLight,
-			nameColor: COLORS.acsentLight
-		};
+  React.useEffect(() => {
+    setTimeout(loadData, 1000);
+  }, []);
 
-	return (
-		<TouchableOpacity style={styles.card} onPress={onPress}>
-			<Image source={user.profilePiC ? { uri: user.profilePiC } : ICONS_LIGHT.userLight} style={styles.image} />
-			<CustomText numberOfLines={1} style={{...styles.userName, color: colorTheme.userColor}}>
-				@{user.userName}
-			</CustomText>
-			<CustomText numberOfLines={2} style={{...styles.name, color: colorTheme.nameColor}}>
-				{user.name}
-			</CustomText>
-		</TouchableOpacity>
-	);
+  const loadData = () => {
+    setData(DATA);
+  };
+  renderData = () =>{
+  return (
+    <TouchableOpacity style={styles.card} onPress={onPress}>
+      <Image
+        source={
+          user.profilePiC ? { uri: user.profilePiC } : ICONS_LIGHT.userLight
+        }
+        style={styles.image}
+      />
+      <CustomText
+        numberOfLines={1}
+        style={{ ...styles.userName, color: colorTheme.userColor }}
+      >
+        @{user.userName}
+      </CustomText>
+      <CustomText
+        numberOfLines={2}
+        style={{ ...styles.name, color: colorTheme.nameColor }}
+      >
+        {user.name}
+      </CustomText>
+    </TouchableOpacity>
+  )};
+  renderLoading = () => {
+    return (
+      <View style={styles.loading}>
+        <Spinner />
+      </View>
+    );
+  };
+  return data.length > 0 ? renderData() : renderLoading();
 });
 const styles = StyleSheet.create({
-	card: {
-		width: 70,
-		height: '100%',
-		alignItems: 'center',
-		// justifyContent:'space-around',
-		marginRight: 10,
-		overflow: 'hidden'
-	},
-	image: {
-		width: 55,
-		height: 55,
-		borderRadius: 40,
-		borderWidth: 3,
-		borderColor: COLORS.commentsColorLight
-	},
-	userName: {
-		fontSize: 9,
-		marginTop: 5
-	},
-	name: {
-		fontSize: 13,
-		textAlign: 'center'
-	}
+  card: {
+    width: 70,
+    height: "100%",
+    alignItems: "center",
+    // justifyContent:'space-around',
+    marginRight: 10,
+    overflow: "hidden",
+  },
+  image: {
+    width: 55,
+    height: 55,
+    borderRadius: 40,
+    borderWidth: 3,
+    borderColor: COLORS.commentsColorLight,
+  },
+  userName: {
+    fontSize: 9,
+    marginTop: 5,
+  },
+  name: {
+    fontSize: 13,
+    textAlign: "center",
+  },
+  loading: {
+	flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+	  marginRight:50
+  }
 });
